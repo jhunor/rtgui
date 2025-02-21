b4cbbbabdb43af728a05ee9c0e125b9cfab86ffd
HJAK <hunorsandor.jakab@infrabel.be>
2025-02-21 07:28:38 +0100

    Dummy model added

---
+++ client.academy.application/src/model/BewegungsdatenModel.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BewegungsdatenModel {
+	
+	private List<String> operatingPoints;
+	private List<String> tracks;
+	/**
+	 * @return the operatingPoints
+	 */
+	public List<String> getOperatingPoints() {
+		return operatingPoints;
+	}
+	/**
+	 * @param operatingPoints the operatingPoints to set
+	 */
+	public void setOperatingPoints(List<String> operatingPoints) {
+		this.operatingPoints = operatingPoints;
+	}
+	/**
+	 * @return the tracks
+	 */
+	public List<String> getTracks() {
+		return tracks;
+	}
+	/**
+	 * @param tracks the tracks to set
+	 */
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+	
+	
+	private void generateDummyData() {
+		this.operatingPoints = new ArrayList<String>(Arrays.asList("A","B","C","D"));
+		this.tracks = new ArrayList<String>(Arrays.asList("12","23","34"));
+	}
+	
+	
+
+}
